c9701dd70cb16784f96713f12ea71bc5c2cb4840
MacroUniverse <2701744082qq.com>
2024-06-10 18:02:00 -0500

    1

---
+++ openblas-0.3.27/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.27")
+SET(OpenBLAS_INCLUDE_DIRS /opt/homebrew/Cellar/openblas/0.3.27/include)
+SET(OpenBLAS_LIBRARIES /opt/homebrew/Cellar/openblas/0.3.27/lib/libopenblas.dylib)
--- setup.sh
+++ setup.sh
@@ -10,5 +10,7 @@ $ADD/flint-3.1.3
 $ADD/gmp-6.3.0
 $ADD/gsl-2.7.1
 $ADD/hdf5-1.14.3
+$ADD/lapack-3.12.0
 $ADD/mpfr-4.2.1
+$ADD/openblas-0.3.27
 $ADD/sqlitecpp-3.2.1
