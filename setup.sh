7da546abc870d2be5710c2d58b89e81cbbd03329
MacroUniverse <2701744082qq.com>
2024-06-10 17:30:25 -0500

    eigen

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ echo ":: initializing SLISC-libs-mcsOS-14.4-m1"
 
 ADD="source lib_prepend.sh $PWD"
 
-$ADD/sqlitecpp-3.2.1
+$ADD/boost-1.85.0
+$ADD/eigen-3.4.0
 $ADD/flint-3.1.3
 $ADD/gmp-6.3.0
-$ADD/mpfr-4.2.1
-$ADD/hdf5-1.14.3
 $ADD/gsl-2.7.1
-$ADD/boost-1.85.0
+$ADD/hdf5-1.14.3
+$ADD/mpfr-4.2.1
+$ADD/sqlitecpp-3.2.1
